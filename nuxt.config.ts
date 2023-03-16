d468ff7b6d4150f3125015e3867a32be3f97c505
Bobby Iliev <bobby@bobbyiliev.com>
2023-03-16 22:28:59 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
-        baseURL: process.env.baseUrl
+        baseURL: process.env.baseUrl,
+        assetPrefix: 'https://lionfish-app-jfo5r.ondigitalocean.app/admin/'
     }
 })
